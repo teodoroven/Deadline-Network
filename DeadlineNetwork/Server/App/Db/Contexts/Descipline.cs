114d04e19b9f493e0f68a4b14ada03e9b3b91503
idinahuymrx9@gmail.com <idinahuymrx9@gmail.com>
2024-02-17 12:15:00 +0500

    sdada

--- DeadlineNetwork/Server/App/Db/Contexts/Descipline.cs
+++ DeadlineNetwork/Server/App/Db/Contexts/Descipline.cs
@@ -10,10 +10,11 @@ public partial class Descipline
     public string? Name { get; set; }
 
     public int GroupId { get; set; }
-
+    
     public string? Comment { get; set; }
 
     public virtual Group Group { get; set; } = null!;
 
     public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+    // Aboba
 }
